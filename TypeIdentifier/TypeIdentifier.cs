a23c43ec7d47643b1d8bedf587ac39fd9df7d681
Stanislav Prigoda <Stanislav.Prigoda@endava.com>
2021-10-11 10:37:43 +0300

    completed this project

--- TypeIdentifier/TypeIdentifier.cs
+++ TypeIdentifier/TypeIdentifier.cs
@@ -6,16 +6,74 @@ namespace TypeIdentifier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                VerifyType();
+            }
+        }
 
-            /*
-                Type refType = studentField.GetType();
-                studValueType = studentField.GetType();
-                TypeCode xtype = Type.GetTypeCode(refType);
-            if (typeof(studentPropertyType) == typeof(int))
-                { }
-                */
+        static void VerifyType()
+        {
+            Console.WriteLine("enter something");
+            string input = Console.ReadLine();
 
+
+            if (Byte.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Unsigned 8-bit integer (byte)");
+            }
+            else if (SByte.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Signed 8-bit integer (sbyte");
+            }
+            else if (Int16.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Signed 16-bit integer (short)");
+            }
+            else if (UInt16.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Unsigned 16-bit integer (ushort)");
+            }
+            else if (Int32.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Signed 32-bit integer (int)");
+            }
+            else if (UInt32.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Unsigned 32-bit integer (uint)");
+            }
+            else if (Int64.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Signed 64-bit integer (long)");
+            }            
+            else if (UInt64.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Unsigned 64-bit integer (ulong)");
+            }
+            else if (Single.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Float");
+            }
+            else if (Double.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Double");
+            }
+            else if (Decimal.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is Decimal");
+            }
+            else if (Boolean.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is boolean");
+            }
+            else if (Char.TryParse(input, out _))
+            {
+                Console.WriteLine($"{input} is char");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is string");
+            }
         }
     }
 }
