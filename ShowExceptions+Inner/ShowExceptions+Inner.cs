ec3c199b291dde3deac0f3ad5c3a4e90227d2726
Stanislav Prigoda <Stanislav.Prigoda@endava.com>
2021-10-11 11:06:29 +0300

    trying to clean up from console

---
+++ ShowExceptions+Inner/ShowExceptions+Inner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShowExceptions_Inner
+{
+    class ShowExceptions_Inner
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- StoreCharactersFromFile/StoreCharactersFromFile.cs
+++ StoreCharactersFromFile/StoreCharactersFromFile.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Win32.SafeHandles;
+
 
 namespace StoreCharactersFromFile
 {
@@ -6,7 +11,55 @@ namespace StoreCharactersFromFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Dictionary<char, int> occurrence = new Dictionary<char, int>();
+            Console.WriteLine("enter path to the file");
+
+            while (true)
+            {                
+                var path = Console.ReadLine();
+
+                if (File.Exists(path))
+                {
+                    using (FileStream fileStream = File.Open(path, FileMode.Open))
+                    {
+                        using (StreamReader streamReader = new StreamReader(fileStream, true))
+                        {
+                            while (streamReader.Peek() >= 0)
+                            {
+                                char localChar = (char)streamReader.Read();
+                                if (!occurrence.TryAdd(localChar, 1))
+                                {
+                                    int count;
+                                    occurrence.TryGetValue(localChar, out count);
+                                    occurrence.Remove(localChar);
+                                    occurrence.Add(localChar, count + 1);
+                                }
+
+                            }
+                        }
+                    }
+
+
+                    Console.WriteLine("  Char :  ,  Number of occurrence :  ");
+                    foreach (var pair in occurrence)
+                    {
+                        Console.Write("  " + pair.Key.ToString() + " , " + pair.Value.ToString() + "  ");
+                    }
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(" Can't find your file. Try again");
+                }
+            }
+
+            Console.WriteLine("The end");
+            while (true)
+            {
+                Console.ReadLine();
+            }
         }
     }
 }
